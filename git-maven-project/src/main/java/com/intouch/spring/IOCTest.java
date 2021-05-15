646f914761ebd95cd046fa910f67074d7c0d788a
haochen <h15374718342@126.com>
2021-05-15 20:22:28 +0800

    lendao 第四次修改

--- git-maven-project/src/main/java/com/intouch/spring/IOCTest.java
+++ git-maven-project/src/main/java/com/intouch/spring/IOCTest.java
@@ -14,5 +14,6 @@ public class IOCTest {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
         System.out.println("第三次修改");
+        System.out.println("第四次修改");
     }
 }
